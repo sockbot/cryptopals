083e63b20e0753793866659a47cbd9badde6ebbe
Andy Chan <github@sockbot.com>
2021-08-08 20:25:13 -0700

    write passing code

--- s1c2/get_xor.py
+++ s1c2/get_xor.py
@@ -1,2 +1,5 @@
 def get_xor(buffer1: bytes, buffer2: bytes) -> bytes:
-    pass
+    if len(buffer1) != len(buffer2):
+        raise ValueError("Input buffers must be of equal size.")
+
+    return bytes([bit1 ^ bit2 for bit1, bit2 in zip(buffer1, buffer2)])
