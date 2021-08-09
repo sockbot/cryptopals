48c1452423ac76f846dbe3e9b6f980263a9abf50
Andy Chan <github@sockbot.com>
2021-08-08 20:12:34 -0700

    change hextob64 to handle bytes instead of strings

--- s1c1/hextob64.py
+++ s1c1/hextob64.py
@@ -1,7 +1,5 @@
 from base64 import b64encode
 
 
-def hextob64(hex: str) -> str:
-    hex_bytes = bytes.fromhex(hex)
-    b64 = b64encode(hex_bytes)
-    return b64.decode()
+def hextob64(hex: bytes) -> bytes:
+    return b64encode(hex)
